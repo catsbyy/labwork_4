2b23198b8e71fa679d1034621a816582d9e98fe7
v-andrusenko <andrusenko.vp1@gmai.com>
2021-04-05 21:53:07 +0300

    update

---
+++ labwork_4_7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace labwork_4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
