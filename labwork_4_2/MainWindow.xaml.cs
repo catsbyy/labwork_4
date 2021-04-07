2c0c75c68c3c492742921af9a7d8e49fc98f61a4
v-andrusenko <andrusenko.vp1@gmai.com>
2021-04-07 23:13:48 +0300

    update

---
+++ labwork_4_2/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace labwork_4_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculation calculation = new Calculation();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Start(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                calculation.x = Convert.ToDouble(TextBoxValueX.Text);
+                calculation.StartCalculation();
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте корректность данных");
+            }
+        }
+
+        private void Button_Reset(object sender, RoutedEventArgs e)
+        {
+            calculation.ResetValues();
+        }
+
+        private void Button_Stop(object sender, RoutedEventArgs e)
+        {
+            calculation.StopCalculation();
+        }
+    }
+    class Calculation : INotifyPropertyChanged
+    {
+        static Mutex mutex = new Mutex();
+
+        static ulong j = 1;
+        static int p = 0;
+        public static double w;
+        public double x { get; set; }
+        private bool IsRunning = true;
+        Thread thread;
+
+        public Calculation()
+        {
+            thread = new Thread(Calculate);
+        }
+        public double W
+        {
+            get { return w; }
+            set
+            {
+                w = value;
+                OnPropertyChanged("w");
+            }
+        }
+
+        public void ResetValues()
+        {
+            StopCalculation();
+            j = 1;
+            p = 0;
+            w = 1;
+        }
+        public void Calculate()
+        {      
+            mutex.WaitOne();
+            w = 1;
+            
+            while(true)
+            {
+                p++;
+                w += j * Math.Pow(Math.Sin(x), p);
+                j *= 2;
+
+                w -= j * Math.Pow(Math.Cos(x), p);
+
+                j *= 2;
+                Thread.Sleep(1000);
+
+                if (!IsRunning)
+                    break;
+            }
+            mutex.ReleaseMutex();
+  
+            //thread.Start();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void StartCalculation()
+        {
+            IsRunning = true;
+            thread.Start();
+        }
+        public void StopCalculation()
+        {
+            IsRunning = false;
+        }
+    }
+    
+}
